1fe6d1fc023e8bfbffff27bccb3d6f6a5d1c0a98
Adesipe Adeola <princeadeola234@gmail.com>
2022-10-14 17:36:19 -0700

    committed

---
+++ app/src/main/java/com/example/fragments/Fragment03.java
@@ -0,0 +1,63 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment03 extends Fragment {
+    private Button btnNavFrag01;
+    private Button btnNavFrag02;
+    private Button btnNavFrag03;
+    private Button nextActivity;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment01, container, false);
+
+        btnNavFrag01 = view.findViewById(R.id.btnNav01);
+        btnNavFrag02 = view.findViewById(R.id.btnNav02);
+        btnNavFrag03 = view.findViewById(R.id.btnNav03);
+        nextActivity = view.findViewById(R.id.nextActivity);
+
+        btnNavFrag01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Moving to Fragment 01", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnNavFrag02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Moving to Fragment 02", Toast.LENGTH_SHORT).show();
+                ((MainActivity)getActivity()).setViewPager(0);
+            }
+        });
+
+        btnNavFrag03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Moving to Fragment 03", Toast.LENGTH_SHORT).show();
+                ((MainActivity)getActivity()).setViewPager(1);
+            }
+        });
+
+        nextActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Moving to new Activity", Toast.LENGTH_SHORT).show();
+                ((MainActivity)getActivity()).setViewPager(2);
+            }
+        });
+
+        return view;
+    }
+}
